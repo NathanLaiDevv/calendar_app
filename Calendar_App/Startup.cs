34dc66c7a1171670034acd248ff1bc61156423f5
Nathan Lai <z5209733@ad.unsw.edu.au>
2020-08-12 18:06:39 +1000

    Add project files.

---
+++ Calendar_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Calendar_App.Startup))]
+namespace Calendar_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
